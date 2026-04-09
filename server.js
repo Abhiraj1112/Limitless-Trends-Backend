5cfadaca74e7b2b3ef8f590573b643bc6b642a1a
Abhiraj <jackson8051302301@gmail.com>
2026-04-10 02:52:00 +0530

    Backend ready for deployment

--- server.js
+++ server.js
@@ -93,6 +93,9 @@ app.get("/", (req, res) => {
   res.send("API Running 🚀");
 });
 
+app.get("/", (req, res) => {
+  res.send("API Running 🚀");
+});
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
